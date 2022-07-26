ff7405af64ff3c841a92a4a00c9d74fc53eb1b4f
Ortal Wikoff <owikoff@gmail.com>
2022-07-26 16:57:40 -0400

    Updated index.js

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongTo(Category);
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongToMany(Tag, {
+  through: ProductTag,
+  foreignKey:'product_id',
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id', 
+})
 
 module.exports = {
   Product,
