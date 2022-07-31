c1b4e7268bdab64a1ce795591e8850ba5aa21a71
Ortal Wikoff <owikoff@gmail.com>
2022-07-31 10:48:34 -0400

    updated tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,35 +24,34 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
   Tag.findOne({
     where: {
-      id: req.params.id, 
+      id: req.params.id,
     },
-    include:[
+    include: [
       {
         model: Product,
-        attributes: ["id", "prduct_name", "price", "stock", "category_id"], 
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
         through: ProductTag,
-        as: "Products", 
+        as: "products",
       },
-    ], 
+    ],
   })
-  .then((dbTagData) => {
-    if (!dbTagData) {
-      res
-      .status(404)
-      .json({ message: "No tag found with this id"}); 
-      return; 
-    }
-    res.json(dbTagData);
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(500).json(err);
-  });
+    .then((dbTagData) => {
+      if (!dbTagData) {
+        res
+          .status(404)
+          .json({ message: "There was no tag found with this id." });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
